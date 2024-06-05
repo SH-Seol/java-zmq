4b767aa621e099065422df8ddad858f3e15e9029
설승환 <tjftmd7@gmail.com>
2024-06-05 16:31:54 +0900

    add zeromq publisher-subscriber pattern

--- src/main/java/zmq/PubSub/client.java
+++ src/main/java/zmq/PubSub/client.java
@@ -1,2 +1,35 @@
-package zmq.PubSub;public class client {
+package zmq.PubSub;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class client {
+    public static void main(String[] args) {
+        try(ZContext context = new ZContext()) {
+            ZMQ.Socket socket = context.createSocket(ZMQ.SUB);
+
+            System.out.println("Colleecting updates from weather server...");
+            socket.connect("tcp://*:5556");
+
+            String zip_filter = (args.length > 0) ? args[0] : "10001";
+            socket.subscribe(zip_filter.getBytes(ZMQ.CHARSET));
+
+            int totalTemp = 0;
+            for(int updateNbr = 0; updateNbr < 20; updateNbr++) {
+                String string = socket.recvStr(0);
+                String[] message = string.split(" ");
+                int zipcode = Integer.parseInt(message[0]);
+                int temperature = Integer.parseInt(message[1]);
+                int relhumidity = Integer.parseInt(message[2]);
+
+                totalTemp += temperature;
+
+                String formattedMessage1 = String.format("Received tempearure for zipcode '%s' was %d F", zip_filter, temperature);
+                String formattedMessage2 = String.format("Average tempearure for zipcode '%s' was %d F", zip_filter, totalTemp/(updateNbr+1));
+
+                System.out.println(formattedMessage1);
+                System.out.println(formattedMessage2);
+            }
+        }
+    }
 }
--- src/main/java/zmq/PubSub/server.java
+++ src/main/java/zmq/PubSub/server.java
@@ -1,2 +1,30 @@
-package zmq.PubSub;public class server {
+package zmq.PubSub;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+import java.util.Random;
+
+public class server {
+    public static void main(String[] args) {
+        System.out.println("Publishing updates at weather server...");
+
+        try(ZContext context = new ZContext()) {
+            ZMQ.Socket socket = context.createSocket(ZMQ.PUB);
+            socket.bind("tcp://*:5556");
+
+            Random rand = new Random();
+            while (!Thread.currentThread().isInterrupted()) {
+                int zipcode = rand.nextInt(1, 100000);
+                int temperature = rand.nextInt(-80, 135);
+                int relhumidity = rand.nextInt(10, 60);
+
+                String message =   String.format("%d %d %d",zipcode,temperature,relhumidity);
+                socket.send(message.getBytes(ZMQ.CHARSET), 0);
+
+                
+            }
+
+        }
+    }
 }
