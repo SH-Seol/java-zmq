99f9b086b649ce9ec2820efe97713caa5e8c07a9
설승환 <tjftmd7@gmail.com>
2024-06-05 17:17:50 +0900

    add Pull-Push pattern

--- src/main/java/zmq/PullPush/client.java
+++ src/main/java/zmq/PullPush/client.java
@@ -1,2 +1,40 @@
-package zmq.PullPush;public class client {
+package zmq.PullPush;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+import java.util.Random;
+
+public class client {
+    public static void main(String[] args) {
+        try(ZContext context = new ZContext()) {
+            ZMQ.Socket subscriber = context.createSocket(ZMQ.SUB);
+            subscriber.subscribe(ZMQ.SUBSCRIPTION_ALL);
+            subscriber.connect("tcp://localhost:5557");
+
+            ZMQ.Socket publisher = context.createSocket(ZMQ.PUSH);
+            publisher.connect("tcp://localhost:5558");
+
+            Random random = new Random(System.currentTimeMillis());
+
+            while (!Thread.currentThread().isInterrupted()) {
+                ZMQ.Poller poller = context.createPoller(1);
+                poller.register(subscriber, ZMQ.Poller.POLLIN);
+
+                if(poller.poll(100) > 0) {
+                    String message = subscriber.recvStr();
+                    System.out.println("I: received message " + message);
+                }
+                else{
+                    int rand = new Random().nextInt(1,100);
+                    if(rand < 10) {
+                        String message = String.valueOf(rand);
+                        publisher.send(message.getBytes(ZMQ.CHARSET), 0);
+                        System.out.println("I: sending message " + message);
+                    }
+
+                }
+            }
+        }
+    }
 }
--- src/main/java/zmq/PullPush/server.java
+++ src/main/java/zmq/PullPush/server.java
@@ -1,2 +1,21 @@
-package zmq.PullPush;public class server {
+package zmq.PullPush;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class server {
+    public static void main(String[] args) {
+        try(ZContext context = new ZContext()){
+            ZMQ.Socket publisher = context.createSocket(ZMQ.PUB);
+            publisher.bind("tcp://*:5557");
+            ZMQ.Socket collector = context.createSocket(ZMQ.PULL);
+            collector.bind("tcp://*:5558");
+
+            while(!Thread.currentThread().isInterrupted()) {
+                String message = collector.recvStr();
+                System.out.println("I: publishing update" + message);
+                publisher.send(message.getBytes(ZMQ.CHARSET), 0);
+            }
+        }
+    }
 }
