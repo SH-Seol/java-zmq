ba38840dcd197efcedde3be7cd943a81f4bb879a
설승환 <tjftmd7@gmail.com>
2024-06-05 14:00:37 +0900

    add files

---
+++ src/main/java/com/example/zmq/ZmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.zmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZmqApplication.class, args);
+    }
+
+}
